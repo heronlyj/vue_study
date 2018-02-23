a170c67507edf7c0e3d951852500f6eae8b1aa2c
heronlyj <heronlyj@gmail.com>
2018-02-23 17:36:01 +0800

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router.ts'
-import store from './store.ts'
+import router from './router'
+import store from './store'
 import './registerServiceWorker.ts'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: h => h(App)
 }).$mount('#app')
